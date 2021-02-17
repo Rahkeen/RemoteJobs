7618c96f6b701c8ae19692956ca1f7c77e70270d
rmarfatia <rmarfatia@pinterest.com>
2021-02-17 11:29:10 -0800

    Initial Commit

---
+++ app/src/main/java/com/remote/remotejobs/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.remote.remotejobs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
